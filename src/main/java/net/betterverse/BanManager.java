8d8f7847c6eb09fcbc6564c85347ed77cbdacd58
David Marby <david@dmarby.se>
2012-07-01 13:53:06 +0200

    Cleaned up, made it close ban status instead of removing line(Had forgotten that before)\, also made it faster.

--- src/main/java/net/betterverse/BanManager.java
+++ src/main/java/net/betterverse/BanManager.java
@@ -6,11 +6,9 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -20,165 +18,153 @@ import org.bukkit.event.player.PlayerPreLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BanManager extends JavaPlugin implements Listener {
-    public static final Logger log = Logger.getLogger("Minecraft");
+
     public static Connection conn;
-    public static BanManager instance = null;
-    
 
-    public static void log(String message) {
-        log(Level.INFO, message);
+    public void log(String message) {
+        getLogger().info(message);
     }
-
-    public static void log(Level level, String message) {
-        log.log(level, String.format("[BanManager] %s", message));
-    }
-
 
     @Override
     public void onDisable() {
-        log("v" + getDescription().getVersion() + " disabled.");
         try {
             conn.close();
         } catch (SQLException e) {
-            log.log(Level.SEVERE, null, e);
+            Logger.getLogger(BanManager.class.getName()).log(Level.SEVERE, null, e);
         }
+        log("v" + getDescription().getVersion() + " disabled.");
     }
+
     @Override
     public void onEnable() {
-        instance = this;
-        if(this.getConfig() == null){
+        if (this.getConfig() == null) {
             this.saveDefaultConfig();
         }
-        log("v" + getDescription().getVersion() + " enabled.");
         getServer().getPluginManager().registerEvents(this, this);
         try {
-            conn = DriverManager.getConnection("jdbc:mysql://"+ this.getConfig().getString("host") + ":"+ this.getConfig().getString("port") +"/"+this.getConfig().getString("database"), this.getConfig().getString("username"), this.getConfig().getString("password"));
-        } catch (SQLException ex) {
+            conn = DriverManager.getConnection("jdbc:mysql://" + this.getConfig().getString("host") + ":" + this.getConfig().getString("port") + "/" + this.getConfig().getString("database"), this.getConfig().getString("username"), this.getConfig().getString("password"));
+            DatabaseMetaData dbm = conn.getMetaData();
+
+            ResultSet tables = dbm.getTables(null, null, "bans", null);
+            if (tables.next() == false) {
+                PreparedStatement st = conn.prepareStatement("CREATE TABLE IF NOT EXISTS `bans` (`id` int(50) NOT NULL AUTO_INCREMENT,`username` varchar(50) NOT NULL,`reason` varchar(5000) NOT NULL,`closed` tinyint(1) NOT NULL DEFAULT '0',`banned_by` varchar(50) NOT NULL,PRIMARY KEY (`id`)) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;");
+                st.executeUpdate();
+            }
+        } catch (SQLException e) {
             log("Please check your configuration!");
         }
-        
-        try {
-          DatabaseMetaData  dbm = conn.getMetaData();
-     
-        ResultSet tables = dbm.getTables(null, null, "bans", null);
-        if (tables.next() == false) {
-               PreparedStatement st = conn.prepareStatement("CREATE TABLE IF NOT EXISTS `bans` (`id` int(50) NOT NULL AUTO_INCREMENT,`username` varchar(50) NOT NULL,`reason` varchar(5000) NOT NULL,`closed` tinyint(1) NOT NULL DEFAULT '0',`banned_by` varchar(50) NOT NULL,PRIMARY KEY (`id`)) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;");
-               st.executeUpdate(); 
-        }
-        } catch (SQLException ex) {
-               //Logger.getLogger(BanManager.class.getName()).log(Level.SEVERE, null, ex);
-                log("Please check your configuration!");
-        }
+        log("v" + getDescription().getVersion() + " enabled.");
     }
-    public BanManager getInstance(){
-        return instance;
-    }
+
     public String searchPlayer(String regex) {
-        List<Player> players = this.getInstance().getServer().matchPlayer(regex);
+        List<Player> players = getServer().matchPlayer(regex);
         if (!players.isEmpty()) {
             return players.get(0).getName();
         } else {
             return regex;
         }
     }
-    @EventHandler
-    public void onPrePlayerLogin(PlayerPreLoginEvent event) { 
-        try{
-                
-                PreparedStatement stmt = conn.prepareStatement("SELECT username FROM bans WHERE `username`=? AND `closed`=?");
-                stmt.setString(1, event.getName());
-                stmt.setBoolean(2, false);
-                ResultSet rs = stmt.executeQuery();
-                if(rs.next()){
-                    event.setResult(PlayerPreLoginEvent.Result.KICK_OTHER);
-                    event.disallow(event.getResult() ,  "You are banned from this server!");
-                }else{
-                    event.setResult(PlayerPreLoginEvent.Result.ALLOWED);
-                    event.allow();
-                }
+
+    public Boolean isBanned(String player) {
+        try {
+            PreparedStatement stmt = conn.prepareStatement("SELECT username FROM bans WHERE `username`=? AND `closed`=?");
+            stmt.setString(1, player);
+            stmt.setBoolean(2, false);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
                 rs.close();
                 stmt.close();
-            } catch (SQLException ex) {
-                BanManager.log.log(Level.SEVERE, null, ex);
+                return true;
+            } else {
+                rs.close();
+                stmt.close();
+                return false;
             }
-    }
-     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-	Player player = null;
-	if (sender instanceof Player) {
-		player = (Player) sender;
+
+        } catch (SQLException e) {
+            Logger.getLogger(BanManager.class.getName()).log(Level.SEVERE, null, e);
+            return null;
         }
-        if(cmd.getName().equalsIgnoreCase("ban")){
-            if (args.length > 1){
-            try{
-                String name = searchPlayer(args[0]);
-                PreparedStatement stmt = conn.prepareStatement("SELECT username FROM bans WHERE `username`=? AND `closed`=?");
-                stmt.setString(1, name);
-                stmt.setBoolean(2, false);
-                Statement st = conn.createStatement();
-                ResultSet rs = stmt.executeQuery();
-                Player target = Bukkit.getServer().getPlayer(name);
-                if(rs.next()){
-                    sender.sendMessage("User is already banned!");
-                }else{
-                    String reason = "";
-                    for(int i = 0; i<args.length-1;i++)
-                    {
-                        reason = reason+" "+args[i+1];
-                    }
-                    String insertStatement = "Insert into bans (username,reason,banned_by) values (?,?,?)";
-                    PreparedStatement prepStmt = conn.prepareStatement(insertStatement);
-                    prepStmt.setString(1, name);
-                    prepStmt.setString(2, reason);
-                    prepStmt.setString(3, sender.getName());
-                    prepStmt.executeUpdate();
-                    if(target != null){
-                        if(target.isOnline()){
-                            Bukkit.getServer().getPlayer(name).kickPlayer("You have been banned! " + reason);
-                         }
-                    }
-                    sender.sendMessage("Banned user "+ name);
 
-                }
-                st.close();
-                rs.close();
-                stmt.close();
-            } catch (SQLException ex) {
-                Logger.getLogger(BanManager.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            return true;
-            }
-	} 
-        else if(cmd.getName().equalsIgnoreCase("unban")){
-            if (args.length == 1){
-            try{
-                String name = searchPlayer(args[0]);
-                PreparedStatement stmt = conn.prepareStatement("SELECT id FROM bans WHERE `username`=? AND `closed`=?");
-                stmt.setString(1, name);
-                stmt.setBoolean(2, false);
-                Statement st = conn.createStatement();
-                ResultSet rs = stmt.executeQuery();
-                Player target = (Bukkit.getServer().getPlayer(name));
-                if(rs.next()){
-                    PreparedStatement prepStmt = conn.prepareStatement("DELETE FROM bans WHERE `id`=?");
-                    prepStmt.setString(1, rs.getString("id"));
-                    prepStmt.executeUpdate();
-                    sender.sendMessage("Unbanned user "+name);
-                }else{
-                    sender.sendMessage("User "+name+ " is not banned!");
-                }
-                
-
-                st.close();
-                rs.close();
-                stmt.close();
-            } catch (SQLException ex) {
-                Logger.getLogger(BanManager.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            return true;
-            }
-	} 
-	return false; 
     }
-    
+
+    public void banUser(String name, String reason, String banned_by) {
+        try {
+            String insertStatement = "Insert into bans (username,reason,banned_by) values (?,?,?)";
+            PreparedStatement prepStmt = conn.prepareStatement(insertStatement);
+            prepStmt.setString(1, name);
+            prepStmt.setString(2, reason);
+            prepStmt.setString(3, banned_by);
+            prepStmt.executeUpdate();
+            prepStmt.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(BanManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void unbanUser(String player) {
+        try {
+            PreparedStatement prepStmt = conn.prepareStatement("UPDATE bans SET `closed`=? WHERE `username`=? AND `closed`=?");
+            prepStmt.setBoolean(1, true);
+            prepStmt.setString(2, player);
+            prepStmt.setBoolean(3, false);
+            prepStmt.executeUpdate();
+            prepStmt.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(BanManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onPrePlayerLogin(PlayerPreLoginEvent event) {
+        if (isBanned(event.getName())) {
+            event.setResult(PlayerPreLoginEvent.Result.KICK_OTHER);
+            event.disallow(event.getResult(), "You are banned from this server!");
+        }
+
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        Player player = null;
+        if (sender instanceof Player) {
+            player = (Player) sender;
+        }
+        if (cmd.getName().equalsIgnoreCase("ban")) {
+            if (args.length > 1) {
+                String name = searchPlayer(args[0]);
+                Player target = this.getServer().getPlayer(name);
+                if (isBanned(name)) {
+                    sender.sendMessage("User is already banned!");
+                } else {
+                    String reason = "";
+                    for (int i = 0; i < args.length - 1; i++) {
+                        reason = reason + " " + args[i + 1];
+                    }
+                    banUser(name, reason, sender.getName());
+                    if (target != null) {
+                        if (target.isOnline()) {
+                            this.getServer().getPlayer(name).kickPlayer("You have been banned! " + reason);
+                        }
+                    }
+                    sender.sendMessage("Banned user " + name);
+
+                }
+                return true;
+            }
+        } else if (cmd.getName().equalsIgnoreCase("unban")) {
+            if (args.length == 1) {
+                String name = searchPlayer(args[0]);
+                Player target = this.getServer().getPlayer(name);
+                if (isBanned(name)) {
+                    unbanUser(name);
+                    sender.sendMessage("Unbanned user " + name);
+                } else {
+                    sender.sendMessage("User " + name + " is not banned!");
+                }
+                return true;
+            }
+        }
+        return false;
+    }
 }
